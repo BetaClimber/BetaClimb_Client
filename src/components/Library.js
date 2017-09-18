0ff5142c7deefc74c1501ad9f3b8c88e487aa9fd
Andrew Eskenazi <drewesk@gmail.com>
2017-09-17 19:45:10 -0600

    Adding the dedicated Library

--- src/components/Library.js
+++ src/components/Library.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 export class Library extends Component {
   render() {
     return (
-      <div className="rendered">
-        <p>this is a library</p>
+      <div className="rendered black">
+        <iframe className='dedicated-library-player' src="https://react-tube-90515.firebaseapp.com/" frameBorder="0" title='Alex Honnold'></iframe>
       </div>
     );
   }
