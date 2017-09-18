842d8377d9789059f07396fd93864601f0453410
Andrew Eskenazi <drewesk@gmail.com>
2017-09-17 19:45:43 -0600

    updating elements and linter error

--- src/components/Note.js
+++ src/components/Note.js
@@ -3,17 +3,17 @@ import React, { Component } from 'react';
 export class Note extends Component {
 
   componentDidMount() {
-
+    console.log(this.props);
   }
 
   render() {
     let note = this.props.note
     return(
       <div className="log-wrapper">
-        <p>{note.blerb}</p>
-        <p>{note.highlights}</p>
-        <p>{note.pitfalls}</p>
-        <p>{note.conditionType}</p>
+        <p>Blerb: {note.blerb}</p>
+        <p>Highlights: {note.highlights}</p>
+        <p>Pitt-Falls: {note.pitfalls}</p>
+        <p>Condition: {note.conditionType}</p>
         <button onClick={ this.props.delete.bind(this, note.id) }>Delete</button>
       </div>
     );
